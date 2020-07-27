18193fcfe6ab219ac1614a2c9bf952b109cb2faa
daoudnazek <dawood_nazek_92@hotmail.com>
2020-07-28 01:03:04 +0300

    complete location

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const server = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 const { response, request } = require('express');
 require('dotenv').config();
 const app = server();
@@ -9,21 +10,32 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3500;
 
-app.get('/location', (request, response) => {
-    const data = require('./data/location.json');
+app.get('/location', getLocation);
+
+function getLocation(request, response) {
     let city = request.query.city;
-    let locationData = new Location(city, data);
-    response.status(200).send(locationData);
-})
+    locationData(city).then(returnedData => {
+        response.status(200).send(returnedData);
+    })
+
+};
+
+function locationData(city) {
+    let APIKEY = process.env.GEOCODE_API_KEY;
+    let url = `https://eu1.locationiq.com/v1/search.php?key=${APIKEY}&q=${city}&format=json`;
+    return superagent.get(url).then(data => {
+        let locationData = new Location(city, data.body);
+        return locationData;
+    })
+};
 
 app.get('/weather', (request, response) => {
     const dataOfWeather = require('./data/weather.json');
     let weatherArr = dataOfWeather.data.map(e => new Weather(e));
-    console.log(weatherArr);
     response.status(200).send(weatherArr);
 })
 
-app.all('*', (request,response) => {
+app.all('*', (request, response) => {
     response.status(500).send('this page doesn`t exist !!');
 })
 
